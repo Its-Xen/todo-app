853f3eeb340df51f5b617a98bf4b65a15d76c275
tonayi <parsatonayi5@gmail.com>
2022-05-20 19:00:04 +0430

    initial commit

--- data/urls.py
+++ data/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name = "home")
+    path('todo/', home, name = "home"),
+    path('add/', Add, name = "ADDTODO"),
+    path('delete/<int:pk>/', delete, name = "deletetodo")
 ]
--- data/views.py
+++ data/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 from .models import todo
 
 def home(request):
     task = todo.objects.all()
     context = {"task" : task}
     return render(request, "home.html", context)
+
+
+def Add(request):
+    new_item = todo(task = request.POST['task'])
+    new_item.save()
+    return HttpResponseRedirect('/todo/')
+
+def delete(request, pk):
+    del_item = todo.objects.get(id = pk)
+    del_item.delete()
+    return HttpResponseRedirect('/todo/')
